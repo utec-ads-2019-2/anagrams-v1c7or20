a6a8d4ad0cacfed077564218bef6f245ddd33618
v1c7or20 <victorpa793@gmail.com>
2019-08-24 21:03:27 -0500

    completed

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
-
+#include <vector>
+#include <map>
+#include <algorithm>
 using namespace std;
 
+string minuscula(string palabra){
+    for (int i = 0; i < palabra.size(); i++) {
+        palabra[i]= tolower(palabra[i]);
+    }
+    sort(palabra.begin(),palabra.end());
+    return palabra;
+}
+
 int main() {
+    vector<string> data;
+    map<string,unsigned int> repeticiones;
+    string dato;
+    cin>>dato;
+    while (dato[0] != '#'){
+        data.push_back(dato);
+        repeticiones[minuscula(dato)];
+        repeticiones[minuscula(dato)]++;
+        cin>>dato;
+    }
+    sort(data.begin(),data.end());
+    for (int j = 0; j < data.size(); j++) {
+        if ((repeticiones)[minuscula((data)[j])]==1){
+            cout<<data[j]<<endl;
+        }
+    }
 	return EXIT_SUCCESS;
 }
 
